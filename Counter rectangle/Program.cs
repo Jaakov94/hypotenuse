e98cb229b697e3f78bfc3ba5908538ad45930ab5
Dell <Dell@DELL-KOMPUTER>
2022-06-26 13:34:48 +0200

    Dodaj pliki projektów.

---
+++ Counter rectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartości przyprostokątnych");
+            Console.WriteLine("A= ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("B= ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Math.Sqrt(a * a + b * b);
+            if (a>0 && b>0)
+            {
+                Console.WriteLine("Przeciwprostokątna ma długość: " + c);                
+            }
+            else
+            {
+                Console.WriteLine("a i b muszą być większe od 0");
+             }
+        }
+    }
+}
+    
+       
+ 
